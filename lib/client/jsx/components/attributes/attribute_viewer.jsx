ad6d42660c63ddf50cca406b66a3ed1bd6e6af1e
Jason Cater <jasondcater@gmail.com>
2018-07-23 20:47:33 -0700

    Adding a AE component.

--- lib/client/jsx/components/attributes/attribute_viewer.jsx
+++ lib/client/jsx/components/attributes/attribute_viewer.jsx
@@ -7,6 +7,7 @@ import TableAttribute from './table_attribute';
 // Standard attributes.
 import {AttributeContainer as Attribute} from './attribute';
 import {CheckboxAttributeContainer as CheckboxAttribute} from './checkbox_attribute';
+import {AdverseEventAttributeContainer as AdverseEventAttribute} from './adverse_event_attribute';
 import {ClinicalAttributeContainer as ClinicalAttribute} from './clinical_attribute';
 import {CollectionAttributeContainer as CollectionAttribute} from './collection_attribute';
 import {DateTimeAttributeContainer as DateTimeAttribute} from './date_time_attribute';
@@ -52,8 +53,10 @@ export default class AttributeViewer extends React.Component{
       case 'DemographicAttribute':
       case 'DiagnosticAttribute':
       case 'TreatmentAttribute':
+        return <ClinicalAttribute {...this.props} />;
       case 'AdverseEventAttribute':
-        return <ClinicalAttribute  {...this.props} />;
+        return <TableAttribute {...this.props} />;
+      //  return <AdverseEventAttribute {...this.props} />;
 
       case 'BoxPlotAttribute':
         return <BoxPlotAttribute {...this.props} />;
