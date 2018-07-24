3cdd7a0ab7d944524e94bf0e6f2ff785e0334430
Jason Cater <jasondcater@gmail.com>
2018-07-23 19:32:57 -0700

    Adding the serach component to render the individual cells.

---
+++ lib/client/jsx/components/search/search_table_cell.jsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import AttributeViewer from '../attributes/attribute_viewer';
+
+export default class SearchTableCell extends React.Component{
+
+  render(){
+    let {
+      document,
+      template,
+      record_name,
+      attr_name
+    } = this.props;
+
+    var attr_viewer = null;
+    if(template.attributes[attr_name].attribute_class!='Magma::TableAttribute'){
+
+      var attr_viewer_props = {
+        'mode': this.props.mode,
+        'template': template,
+        'document': this.props.document,
+        'value': document[attr_name],
+        'attribute': template.attributes[attr_name]
+      };
+
+      attr_viewer = <AttributeViewer {...attr_viewer_props} />;
+    }
+
+    return(
+      <td className='table-viewer-cell'>
+
+        {attr_viewer}
+      </td>
+    );
+  }
+}
