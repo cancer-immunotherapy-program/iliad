74cd5e2a5df7fc2d00ef210b7abd1b087a79462f
Jason Cater <jasondcater@gmail.com>
2018-12-06 18:47:59 -0800

    Refactoring the clinical attribute to reuse patterns.

--- lib/client/jsx/components/attributes/clinical_attribute.jsx
+++ lib/client/jsx/components/attributes/clinical_attribute.jsx
@@ -8,39 +8,40 @@ import ClinicalInput from '../inputs/clinical_input';
 // Module imports.
 import {
   sendRevisions,
-  reviseDocument
+  reviseTable
 } from '../../actions/magma_actions';
 
 import {
   selectModelDocuments,
-  selectDictionaryByModel
+  selectDictionaryByModel,
+  selectModelRevisions
 } from '../../selectors/magma_selector';
 
 import {
   nestDataset,
-  setDefinitionUids,
-  setSiblingUids
+  setDefinitionUids
 } from '../../selectors/selector_utils.js';
+
+/*
+ * There are three main data structures in the Clinical Attribute that keep
+ * the data organized. The 'records' is the data from the DB in a fairly
+ * flat object. The 'dictionary' is the data object that helps set the
+ * validation on the possible values the records can take. The 'nested_uid'
+ * is a mapping of the 'records' and 'dictionary'. The 'nested_uid' object
+ * contains only the keys from the 'record' and 'dictionary'.
+ *
+ * 'records' can be nested and can have a hierarchy and this can be
+ * difficult to work with. To get the best of both worlds (a flat set of
+ * records and a tree of relations) the 'nested_uid' data structure holds
+ * the relations of the 'records' and the mappings of the 'dictionary'
+ * validations separate from the 'records' themselves.
+ */
 
 export class ClinicalAttribute extends React.Component{
   constructor(props){
     super(props);
 
-    /*
-     * There are three main data structures in the Clinical Attribute that keep
-     * the data organized. The 'records' is the data from the DB in a fairly
-     * flat object. The 'dictionary' is the data object that helps set the
-     * validation on the possible values the records can take. The 'nested_uid'
-     * is a mapping of the 'records' and 'dictionary'. The 'nested_uid' object
-     * contains only the keys from the 'record' and 'dictionary'.
-     *
-     * 'records' can be nested and can have a hierarchy and this can be
-     * difficult to work with. The 'nested_uid' data structure holds the
-     * relations of the 'records' and the mappings of the 'dictionary'
-     * validations.
-     */
     this.state = {
-      dictionary: {},
       nested_uids: []
     };
   }
@@ -54,37 +55,8 @@ export class ClinicalAttribute extends React.Component{
     ) return null;
 
     return {
-      //records: next_props.records,
-      dictionary: next_props.dictionary,
       nested_uids: next_props.nested_uids
     };
-  }
-
-  sendRevisionIntermediate(){
-    /*
-    let revisions = {}
-    let records = this.state.records;
-    for(let uid in records){
-      if(records[uid].revised == true){
-
-        revisions[uid] = {
-          uid: uid,
-          parent_uid: records[uid].parent_uid,
-          subject_id: this.props.document.subject_id,
-          name: records[uid].name,
-          value: records[uid].value,
-          restricted: true
-        };
-      }
-    }
-
-    let revision_args = {
-      model_name: this.props.attribute.name,
-      revisions
-    };
-
-    this.props.sendRevisions(revision_args);
-    */
   }
 
   // Extract the defintions required for key inputs.
@@ -92,7 +64,7 @@ export class ClinicalAttribute extends React.Component{
     let definitions = {};
 
     uids.forEach((uid)=>{
-      let def = this.state.dictionary.definitions[uid];
+      let def = this.props.dictionary.definitions[uid];
 
       if(def == undefined){
         debugger;
@@ -112,303 +84,131 @@ export class ClinicalAttribute extends React.Component{
   // Extract the definitions required for value inputs.
   selectDefinitionsForValues(uids){
     return uids.map((uid)=>{
-      let def = Object.assign({}, this.state.dictionary.definitions[uid]);
+      let def = Object.assign({}, this.props.dictionary.definitions[uid]);
       def['label'] = def.value;
       return def;
     });
   }
 
-  selectSiblings(parent_uid){
-    let definitions = this.state.dictionary.definitions;
-    let siblings = [];
-
-    for(let uid in definitions){
-      if(parent_uid != definitions[uid].parent_uid) continue;
-      siblings.push(uid);
-    }
-
-    return siblings;
-  }
-
-  setNestedUidRevision(rev_uid, revision, nested_uids){
-    nested_uids.forEach((nested_uid, index)=>{
-      if(rev_uid == nested_uid.uid){
-        nested_uids[index] = Object.assign({}, nested_uid, revision);
-      }
-
-      if('children' in nested_uid){
-        nested_uid.children = this.setNestedUidRevision(
-          rev_uid,
-          revision,
-          nested_uids[index].children
-        );
-      }
-    });
-
-    return nested_uids;
-  }
-
-  nestedUidHasChildren(orig_uid, nested_uids){
-    let has_children = false;
-    nested_uids.forEach((nested_uid)=>{
-
-      if(nested_uid.children.length > 0){
-        if(orig_uid == nested_uid.uid){
-          return true;
-        }
-        else{
-          has_children = this.nestedUidHasChildren(
-            orig_uid,
-            nested_uid.children
-          );
-        }
-      }
-    });
-
-    return has_children;
-  }
-
-  setRecordRevision(uid, revision, records){
-    records[uid] = Object.assign({}, records[uid], revision);
-    return records;
-  }
-
-  setChildren(def_parent_uid, rec_parent_uid){
-    let child_recs = [];
-    let child_defs = [];
-    let child_siblings = [];
-    let defs = this.state.dictionary.definitions;
-
-    for(let def_uid in defs){
-      if(defs[def_uid].parent_uid == def_parent_uid){
-
-        // Add to the sibling grouping.
-        child_siblings.push(def_uid);
-
-        // For each unique named definition we add a child record.
-        if(!(defs[def_uid].name in child_defs)){
-
-          let random_uid = `new-${Math.random().toString(16).slice(2, -1)}`
-
-          // Set initial child nested ids.
-          child_defs[defs[def_uid].name] = {
-            uid: random_uid,
-            definition: null,
-            definitions: [def_uid],
-            children: []
-          };
-
-          // Set unique child record.
-          child_recs.push({
-            uid: random_uid,
-            parent_uid: rec_parent_uid,
-            name: defs[def_uid].name,
-            revised: true,
-            value: null
-          });
-        }
-        else{
-
-          // Append the definion uid to the child.
-          child_defs[defs[def_uid].name].definitions.push(def_uid);
-        }
-      }
-    }
-
-    // Add the siblings and definitions to the child node.
-    let new_child_defs = []
-    for(let name in child_defs){
-      child_defs[name]['siblings'] = child_siblings;
-      new_child_defs.push(child_defs[name]);
-    }
-
-    let child_uids = {
-      uid: rec_parent_uid,
-      children: new_child_defs
-    };
-    return [child_recs, child_uids];
-  }
-
   reviseRecord(event){
-    let revision_records = {
-      uid: event.target.dataset.uid,
-      value: event.target.value,
-      revised: true
-    };
+    let {attribute, records, reviseTable} = this.props;
+    this.addChildRecordsProxie('value', event.target);
+    records[event.target.dataset.uid]['value'] = event.target.value;
+    reviseTable(attribute.model_name, records);
+  }
 
-    // Pull the definition uid for the revised data.
-    let defs = this.state.dictionary.definitions;
-    let def = Object.keys(defs).filter((uid)=>{
-      if(
-        defs[uid].name == event.target.dataset.name &&
-        defs[uid].value == event.target.value
-      ) return defs[uid];
-    });
-
-    // Make sure the definition is properly set on the revision.
-    let revision_uids = {
-      uid: event.target.dataset.uid,
-      definition: def[0],
-    }
-
-    // Update the actual record.
-    let records = this.setRecordRevision(
-      event.target.dataset.uid,
-      revision_records,
-      this.props.records
-    );
-
-    // Update the nested_uid object.
-    let nested_uids = this.setNestedUidRevision(
-      event.target.dataset.uid,
-      revision_uids,
-      this.state.nested_uids
-    );
-
-    // If there are no definitions here then there should be no children.
-    if(def.length > 0){
-
-      // The data to update for the children.
-      let children = this.setChildren(def[0], event.target.dataset.uid);
-      let child_records = children[0];
-      let child_uids = children[1];
-
-      if(!this.nestedUidHasChildren(child_uids.uid, nested_uids)){
-
-        /*
-         * If we have child records to add loop them and assign them to the main
-         * record object by uid.
-         */
-        child_records.forEach((child_record)=>{
-          records = Object.assign(
-            records,
-            {[child_record.uid]: child_record}
-          );
-        });
-
-        nested_uids = this.setNestedUidRevision(
-          event.target.dataset.uid,
-          child_uids,
-          nested_uids
-        );
-      }
-    }
-
-    this.setState({nested_uids});
-    reviseDocument(
-      this.props.document,
-      this.props.template,
-      this.props.attribute,
-      records
-    );
+  reviseKey(event){
+    let {attribute, records, reviseTable} = this.props;
+    this.addChildRecordsProxie('key', event.target);
+    records[event.target.dataset.uid]['name'] = event.target.value;
+    reviseTable(attribute.model_name, records);
   }
 
   /*
-   * The individual entries are made of key/record pairs. Here we set the key
-   * which should set the definition for the value (which allows validation).
+   * When add children to a record we have a few considerations. If the record
+   * has multiple selections we first must wait until a option/value is
+   * selected. If the record only has one input (i.e. date, sttring, or number)
+   * we will know at the time of key selection which children are next and
+   * required.
    */
-  reviseKey(event){
-    let {doucment, template, attribute, records} = this.props;
-    let {nested_uids} = this.state;
+  addChildRecordsProxie(revision_type, elem){
+    let option_uid = null;
 
-    // Set the name of the definition we chose.
-    let revision_records = {
-      uid: event.target.dataset.uid,
-      name: event.target.value
-    };
+    switch(revision_type){
+      case 'key':
+        // The 'option_uid' is also the dictionary definition uid we select for.
+        option_uid = elem.options[elem.selectedIndex].dataset.option_uid;
 
-    // Extract the id's for matching definitions.
-    let defs = this.state.dictionary.definitions;
-    let revision_uids = {
-      uid: event.target.dataset.uid,
-      definition: null,
-      definitions: Object.keys(defs).filter((uid)=>{
-        if(defs[uid].name == event.target.value) return uid;
-      })
-    };
+        let definitions = this.props.dictionary.definitions;
+        let def = definitions[option_uid];
 
-    /*
-     * If there is a single definition it means that this is not a multi-select.
-     * Which also means we can set the sub children.
-     */
-    let child_records = [];
-    let child_uids = {};
-    if(revision_uids.definitions.length == 1){
-      revision_uids.definition = revision_uids.definitions[0];
+        let def_count = Object.keys(definitions).reduce((acc, key)=>{
+          if(definitions[key].name == elem.value) ++acc;
+          return acc;
+        }, 0);
 
-      let children = this.setChildren(
-        revision_uids.definitions[0],
-        event.target.dataset.uid
-      );
+        if(
+          (def.type != 'dropdown' || def.type != 'checkbox') &&
+          def_count == 1
+        ) this.addChildRecords(option_uid, elem.dataset.uid);
 
-      child_records = children[0];
-      child_uids = children[1];
+        break;
+      case 'value':
+
+        switch(elem.getAttribute('type')){
+          case 'search':
+
+            let list_id = elem.getAttribute('list');
+            let list_options = document.getElementById(list_id).options;
+            for(let a = 0; a < list_options.length; ++a){
+              if(list_options[a].innerText == elem.value){
+                option_uid = list_options[a].dataset.option_uid;
+                break;
+              }
+            }
+
+            this.addChildRecords(option_uid, elem.dataset.uid);
+            break;
+          case 'dropdown':
+            option_uid = elem.options[elem.selectedIndex].dataset.option_uid;
+            this.addChildRecords(option_uid, elem.dataset.uid);
+            break;
+        }
+
+        break;
     }
-
-    records = this.setRecordRevision(
-      event.target.dataset.uid,
-      revision_records,
-      this.props.records
-    );
-
-    nested_uids = this.setNestedUidRevision(
-      event.target.dataset.uid,
-      revision_uids,
-      this.state.nested_uids
-    );
-
-    /*
-     * If we have child records to add loop them and assign them to the main
-     * record object by uid.
-     */
-    child_records.forEach((child_record)=>{
-      records = Object.assign(
-        records,
-        {[child_record.uid]: child_record}
-      );
-    });
-
-    if(child_uids){
-      nested_uids = this.setNestedUidRevision(
-        event.target.dataset.uid,
-        child_uids,
-        nested_uids
-      );
-    }
-
-    this.setState({nested_uids});
-    reviseDocument(document, template, attribute, records);
   }
 
-  addRecord(){
+  addChildRecords(parent_def_uid, parent_rec_uid){
+
+    /*
+     * From the dictionary separate all the definitions with the same
+     * 'parent_def_uid' and group the definitions by name/term.
+     */
+
+    let {definitions} = this.props.dictionary;
+    let next_def_set = {};
+
+    // Loop over all of the definitions.
+    for(let uid in definitions){
+      let def = definitions[uid];
+
+      /*
+       * Select out the definitions whos 'parent_uid' match our current
+       * 'parent_def_uid'.
+       */
+      if(def.parent_uid == parent_def_uid){
+
+        // Group the definitons by name.
+        if(!(def.name in next_def_set)) next_def_set[def.name] = [];
+        next_def_set[def.name].push(def);
+      }
+    }
+
+    // Loop the sorted names and add the child records.
+    for(let name in next_def_set){
+      this.addRecord(name, parent_rec_uid, null);
+    }
+  }
+
+  // This will proxie for an 'add' onClick.
+  addRecordProxie(event){
+    this.addRecord(null, null, null);
+  }
+
+  addRecord(name = null, parent_uid = null, value = null){
     let random_uid = `new-${Math.random().toString(16).slice(2, -1)}`;
-    let {doucment, template, attribute, records} = this.props;
+    let {attribute, records, reviseTable} = this.props;
 
-    records[random_uid] = {
-      uid: random_uid,
-      name: null,
-      parent_uid: null,
-      value: null,
-      revised: false
-    };
-
-    let nested_uids = this.state.nested_uids;
-    nested_uids.unshift({
-      uid: random_uid,
-      siblings: this.selectSiblings(null),
-      definitions: [],
-      definition: null,
-      children: []
-    });
-
-    this.setState({nested_uids});
-    reviseDocument(document, template, attribute, records);
+    records[random_uid] = {uid: random_uid, name, parent_uid, value};
+    reviseTable(attribute.model_name, records);
   }
 
   removeRecord(uid){
+/*
     if(!confirm('Are you sure you want to delete this record?')) return;
 
-    let {document, template, attribute, records} = this.props;
+    let {attribute, records, reviseTable} = this.props;
     let nested_uids = this.state.nested_uids;
 
     if(uid in records){
@@ -419,7 +219,8 @@ export class ClinicalAttribute extends React.Component{
     }
 
     this.setState({nested_uids});
-    reviseDocument(document, template, attribute, records);
+    reviseTable(attribute.model_name, records);
+*/
   }
 
   renderRemoveBtn(is_parent, uid){
@@ -504,20 +305,17 @@ export class ClinicalAttribute extends React.Component{
   }
 
   renderData(){
-    let {records} = this.props;
-    let {dictionary, nested_uids} = this.state;
-
     // Check that all the required data to render is present.
     if(
-      Object.keys(records).length <= 0 ||
-      Object.keys(dictionary).length <= 0 ||
-      Object.keys(dictionary.definitions).length <= 0
+      Object.keys(this.props.records).length <= 0 ||
+      Object.keys(this.props.dictionary).length <= 0 ||
+      Object.keys(this.props.dictionary.definitions).length <= 0
     ) return <div>{'No records.'}</div>;
 
     // Loop the records and render them by group.
     let elements = [];
-    nested_uids.forEach((nested_uid)=>{
-      elements.push(this.renderRecord(nested_uid, true));
+    this.state.nested_uids.forEach((uid_set)=>{
+      elements.push(this.renderRecord(uid_set, true));
     })
 
     return elements;
@@ -528,15 +326,8 @@ export class ClinicalAttribute extends React.Component{
 
     let add_btn_props = {
       className: 'clinical-record-btn',
-      onClick: this.addRecord.bind(this)
+      onClick: this.addRecordProxie.bind(this)
     };
-
-/*
-    let save_btn_props = {
-      className: 'clinical-record-btn',
-      onClick: this.sendRevisionIntermediate.bind(this)
-    };
-*/
 
     return(
       <button {...add_btn_props}>
@@ -597,21 +388,28 @@ export class ClinicalAttribute extends React.Component{
   }
 }
 
-const mapStateToProps = (state, own_props)=>{
+const mapStateToProps = (store, own_props)=>{
   /*
    * The main goal of processing the data here is to:
-   * 1. Attach the basic defintions to the documents.
-   * 2. Nest the documents in their proper hierarchy.
+   * 1. Attach the basic defintions to the records.
+   * 2. Nest the records in their proper hierarchy.
    */
   let args = [
-    state,
+    store,
     APP_CONFIG.project_name,
     own_props.attribute.model_name,
     'all'
   ];
 
   let dictionary = selectDictionaryByModel(...args);
-  let records = selectModelDocuments(...args);
+  let revisions = selectModelRevisions(...args);
+  let records = null;
+  if(Object.keys(revisions).length > 0){
+    records = revisions;
+  }
+  else{
+    records = selectModelDocuments(...args);
+  }
 
   /*
    * Trim out 'identifier serch' items. We need fix the identifier search to not
@@ -622,12 +420,15 @@ const mapStateToProps = (state, own_props)=>{
   //}
 
   let uid_set = setDefinitionUids(records, dictionary , {});
-  uid_set = setSiblingUids(records, dictionary, uid_set);
   uid_set = nestDataset(uid_set, 'uid', 'parent_uid');
   uid_set = Object.keys(uid_set).map((key)=>uid_set[key]);
 
+  // New items that have not yet been saved should display on the top.
+  uid_set.sort((a, b)=>(a.uid.startsWith('new-') ? -1 : 1));
+
   return {
-    records: records,
+    records,
+    has_revisions: (Object.keys(revisions).length > 0),
     dictionary: dictionary,
     nested_uids: uid_set
   };
@@ -635,8 +436,8 @@ const mapStateToProps = (state, own_props)=>{
 
 const mapDispatchToProps = (dispatch, own_props)=>{
   return {
-    sendRevisions: (args)=>{
-      dispatch(sendRevisions(args));
+    reviseTable: (model_name, records)=>{
+      dispatch(reviseTable(model_name, records));
     }
   };
 };
